cbb8225eea6b3852a8c3907705a93c3be145eacd
ilgiz <66334696+sirilgiz@users.noreply.github.com>
2024-05-28 21:24:27 +0300

    Первая версия

---
+++ clean_data.py
@@ -0,0 +1,37 @@
+# Импортируем используемые библиотеки
+import os
+import pandas as pd
+import re
+
+# Функция считывает указанный файл и возвращает его в виде объекта dataframe
+def get_xlsx_content(xlsx_path: str):
+    if not(xlsx_path and os.path.isfile(xlsx_path)):
+        raise FileNotFoundError('Исходный файл не найден или передан пустой путь')
+    df = pd.read_excel(xlsx_path)
+    return df
+
+
+# Функция записывает полученный dataframe в указанный xlsx файл
+def write_xlsx_content(df: pd.DataFrame, xlsx_path: str) -> str:
+    if not xlsx_path:
+        raise FileNotFoundError('Невозможно создать выходной файл — задан пустой путь')
+    if os.path.isfile(xlsx_path):
+        if input(f'ВНИМАНИЕ: Файл {xlsx_path} существует и будет перезаписан. Нажмите [Enter], чтобы продолжить')!= '':
+            raise KeyboardInterrupt('Обработка прервана пользователем')
+            return
+    df.to_excel(xlsx_path, index=False)
+    if os.path.isfile(xlsx_path):
+        return xlsx_path
+
+
+# Функция удаляет все нецифровые символы из строки
+def remove_nondigit_char_from_str(s: str):
+    if not isinstance(s, str):
+        return s
+    return re.sub(r'\D', '', s)
+
+
+def clean_xlsx(in_xlsx_path: str, out_xlsx_path: str):
+    df = get_xlsx_content(in_xlsx_path)
+    df_clean = df.map(remove_nondigit_char_from_str)
+    return write_xlsx_content(df_clean, out_xlsx_path)
---
+++ main.py
@@ -0,0 +1,13 @@
+import clean_data as cd
+from clean_data import clean_xlsx
+in_xlsx_path:str = r'phone_numbers.xlsx' # путь к файлу с исходными данными
+out_xlsx_path:str = r'phone_numbers_clean.xlsx' # путь к файлу для записи результата
+
+if __name__ == '__main__':
+    try:
+        result = clean_xlsx(in_xlsx_path,out_xlsx_path) == out_xlsx_path
+        print('РЕЗУЛЬТАТ:', 'OK' if result else 'неОК')
+    except (FileNotFoundError,KeyboardInterrupt) as e:
+        print('ОШИБКА: ', e)
+    finally:
+        print('ОБРАБОТКА ЗАВЕРШЕНА')
